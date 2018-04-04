acd197222474c1fc51f0adae5805f7ff791d263c
test user <you@example.com>
2018-04-04 12:57:18 +0000

    changed code

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,8 @@
-class ntp (
+class ntp(
   String $package_name        = $ntp::params::package_name,
   String $package_ensure      = $ntp::params::package_ensure,
   String $config_name         = $ntp::params::config_name,
-  String $config_file_mode    = $ntp::params::cofnig_file_mode,
+  String $config_file_mode    = $ntp::params::config_file_mode,
   Array[String] $servers      = $ntp::params::servers,
   String $service_ensure      = $ntp::params::service_ensure,
   String $service_name        = $ntp::params::service_name,
